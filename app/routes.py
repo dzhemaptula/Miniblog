4fc5b6912016d9b780eba000dcb4034300590c5a
Dzhem <dzhem.aptula@gmail.com>
2019-10-02 14:50:55 +0200

    Change linting to flake8 and black and refactor whole project

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,10 @@
-from flask import render_template
 from app import app
 from app.forms import LoginForm
-from flask import render_template, flash, redirect
-
+from flask import render_template, flash, redirect, url_for
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Dzhem'}
+    user = {'username': 'Dzhem'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,12 +15,14 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html',title='Home',user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}. remember_me={}'.format(form.username.data,form.remember_me.data))
-        return redirect('/index')
+        flash('Login requested for user {}. remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
