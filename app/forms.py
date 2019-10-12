0aee584c5057da99f307541d184cd0722fb9cc83
Dzhem <dzhem.aptula@gmail.com>
2019-10-12 20:19:25 +0200

    Add more functionality to edit profile to counter errors

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, TextAreaField, ValidationError
 from wtforms.validators import DataRequired
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -29,3 +30,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     about_me = TextAreaField('About me', [validators.Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    # Checks if user picked the same original name, then the name is unchanged
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
